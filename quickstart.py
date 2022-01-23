6e6f7f5db37e68937e21f63902b2176a5ca59bef
raspberryPi <thisisaraspberrypi>
2022-01-23 11:30:18 -0500

    a

--- quickstart.py
+++ quickstart.py
@@ -27,8 +27,8 @@ from googleapiclient.errors import HttpError
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
 
 # The ID and range of a sample spreadsheet.
-SAMPLE_SPREADSHEET_ID = '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms'
-SAMPLE_RANGE_NAME = 'Class Data!A2:E'
+SAMPLE_SPREADSHEET_ID = '1bTu9VOY2n-gD3ztjfsf2LX2JSrBFXyJ_F1sJxS-JC6E'
+SAMPLE_RANGE_NAME = 'Sheet1!A2:E'
 
 
 def main():
---
+++ url.sh
@@ -0,0 +1,1 @@
+wget https://accounts.google.com/o/oauth1/auth?response_type=code&client_id=343565763292-chssp60itqv400n8k3ln3sn0nca2pek3.apps.googleusercontent.com&redirect_uri=http%3A%2F%2Flocalhost%3A59877%2F&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fspreadsheets.readonly&state=VuOwhinDIilnKwT4tmc3X8FU4JD77g&access_type=offline
